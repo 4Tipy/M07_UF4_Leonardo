243d85cdca97a0764afc566517b86883d780ff3d
4Tipy <lchavez21@jaumebalmes.net>
2023-04-20 19:56:31 +0200

    modelo hecho

--- JaumeBalmes/JaumeBalmes_Leonardo/Practica/models.py
+++ JaumeBalmes/JaumeBalmes_Leonardo/Practica/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class Person(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
+    apellido2 = models.CharField(max_length=50, default='')
     año = models.IntegerField()
-
+    edad = models.PositiveIntegerField()
+    email = models.EmailField()
 
 
